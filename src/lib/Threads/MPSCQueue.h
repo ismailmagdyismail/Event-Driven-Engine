be81e8a9145e7843991598c42171bb9b3c1bd81d
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail00882@yahoo.com>
2026-07-23 22:35:26 +0300

    [BugFix][MPSC_Queue]: fix queue race condition to, re-design it to work with mutliple producer, previously had race condition in write position

--- src/lib/Threads/MPSCQueue.h
+++ src/lib/Threads/MPSCQueue.h
@@ -9,71 +9,89 @@ template <typename T>
 class MPSCQueue
 {
 public:
+    static const unsigned int CACHE_LINE_SIZE = 64;
+
+    struct Slot
+    {
+        alignas(MPSCQueue::CACHE_LINE_SIZE) std::atomic<unsigned int> m_uiVersion;
+        T TObject;
+    };
+
     MPSCQueue(unsigned int maxSize)
     {
         m_uiMaxSize = maxSize;
         m_bIsReading.store(true, std::memory_order_relaxed);
         m_bIsWriting.store(true, std::memory_order_relaxed);
-        m_uiSize.store(0, std::memory_order_relaxed);
-        m_uiReadPosition = 0;
-        m_uiWritePosition = 0;
-        m_vecCircularBuffer.resize(maxSize, T{});
+        m_uiHead = 0;
+        m_uiTail.store(0, std::memory_order_relaxed);
+        m_vecCircularBuffer.reserve(maxSize);
+        for (unsigned int i = 0; i < maxSize; ++i)
+        {
+            auto pSlot = std::make_unique<Slot>();
+            pSlot->m_uiVersion.store(i, std::memory_order_relaxed);
+            pSlot->TObject = T{};
+            m_vecCircularBuffer.push_back(std::move(pSlot));
+        }
     }
 
     bool Push(T object)
     {
-        bool bIsWriting = true;
-        while ((bIsWriting = m_bIsWriting.load(std::memory_order_relaxed)))
-        {
-            //! Read the most up to date size / snapshot
-            //! TRY updating it by reserving
-            unsigned int uiSizeSnapshot = m_uiSize.load(std::memory_order_relaxed);
-
-            //! 1. Has no space, we yield till consumer pop off
-            if (uiSizeSnapshot >= m_uiMaxSize)
-            {
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                continue;
-            }
-
-            //! 2. Has space
-            //! 2.1 another producer(s) may have beaten us to the slot we expect to reserve
-            //! 2.2 no producers beat use we can insert safely
-            unsigned int uiSizeToUpdate = uiSizeSnapshot + 1;
-            if (!m_uiSize.compare_exchange_strong(uiSizeSnapshot, uiSizeToUpdate, std::memory_order_release))
-            {
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                continue;
-            }
-            //! 2.2 if succeeded to break out of the loop, it means we owns the index, can write to it
-            m_vecCircularBuffer[m_uiWritePosition] = object;
-            UpdateIndex(m_uiWritePosition);
-            return true;
-        }
-        return bIsWriting;
-    }
-
-    bool Pop(T &readObject)
-    {
-        //! Since only one reader, we can safely load readPosition and cache it early on and avoid re-loading it again
-        bool bIsReading = true;
-        while (m_uiSize.load(std::memory_order_acquire) == 0 && (bIsReading = m_bIsReading.load(std::memory_order_relaxed)))
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
-
-        if (!bIsReading)
+        if (!m_bIsWriting.load(std::memory_order_relaxed))
         {
             return false;
         }
 
-        readObject = std::move(m_vecCircularBuffer[m_uiReadPosition]);
-        UpdateIndex(m_uiReadPosition);
+        //! 1. Race to Reserve a Logical slot
+        unsigned int uiLogicalPosition = m_uiTail.fetch_add(1, std::memory_order_relaxed);
 
-        //! No need for a two way barrier here, since writers don't acces Written Elements / refrence them
-        //! They just place them in the queue without processing
-        m_uiSize.fetch_sub(1, std::memory_order_relaxed);
+        //! 2. Map it to physical location
+        unsigned int uiPhysicalPosition = GetPositionWithinBounds(uiLogicalPosition);
 
+        bool bReading = true;
+        unsigned int uiVersion = 0;
+        //! 3. wait till that physical location is actually supposed to be written to by that logical slot
+        //! in other words wait till physical == logical
+        //! this is to avoid overwriting data that are yet to be consumed
+        //! when consumed logical version counter is incremented so it increases till expected logical producer counter
+        do
+        {
+            bReading = m_bIsReading.load(std::memory_order_relaxed);
+            uiVersion = m_vecCircularBuffer[uiPhysicalPosition]->m_uiVersion.load(std::memory_order_acquire);
+        } while (bReading && uiVersion < uiLogicalPosition);
+
+        if (!bReading)
+        {
+            return false;
+        }
+
+        // 4. Insert into circular buffer
+        m_vecCircularBuffer[uiPhysicalPosition]->TObject = std::move(object);
+
+        // 5. publish to be seen by consumer
+        m_vecCircularBuffer[uiPhysicalPosition]->m_uiVersion.store(uiLogicalPosition + 1, std::memory_order_release);
+
+        return true;
+    }
+
+    bool Pop(T &readObject)
+    {
+        bool bReading = true;
+        unsigned int uiPhysicalReadLocation = GetPositionWithinBounds(m_uiHead);
+        bool bHasElementToRead = true;
+        do
+        {
+            bReading = m_bIsReading.load(std::memory_order_relaxed);
+            unsigned int uiPublishedVersion = m_vecCircularBuffer[uiPhysicalReadLocation]->m_uiVersion.load(std::memory_order_acquire);
+            bHasElementToRead = (uiPublishedVersion == m_uiHead + 1);
+        } while (!bHasElementToRead && bReading);
+
+        if (!bReading)
+        {
+            return false;
+        }
+        readObject = std::move(m_vecCircularBuffer[uiPhysicalReadLocation]->TObject);
+        m_vecCircularBuffer[uiPhysicalReadLocation]->m_uiVersion.store(m_uiHead + m_uiMaxSize, std::memory_order_release);
+        ++m_uiHead;
         return true;
     }
 
@@ -88,27 +106,17 @@ public:
     }
 
 private:
-    void UpdateIndex(unsigned int &p_iPosition)
+    unsigned int GetPositionWithinBounds(unsigned int idx)
     {
-        unsigned int uiNextPosition = p_iPosition + 1;
-        if (uiNextPosition >= m_uiMaxSize)
-        {
-            p_iPosition = 0;
-        }
-        else
-        {
-            ++p_iPosition;
-        }
+        return idx % m_uiMaxSize;
     }
 
-    static const unsigned int CACHE_LINE_SIZE = 64;
-
+    //! Prevent false sharing
     alignas(MPSCQueue::CACHE_LINE_SIZE) unsigned int m_uiMaxSize;
-    alignas(MPSCQueue::CACHE_LINE_SIZE) unsigned int m_uiReadPosition;
-    alignas(MPSCQueue::CACHE_LINE_SIZE) unsigned int m_uiWritePosition;
-    alignas(MPSCQueue::CACHE_LINE_SIZE) std::atomic<unsigned int> m_uiSize;
+    alignas(MPSCQueue::CACHE_LINE_SIZE) unsigned int m_uiHead;
+    alignas(MPSCQueue::CACHE_LINE_SIZE) std::atomic<unsigned int> m_uiTail;
     alignas(MPSCQueue::CACHE_LINE_SIZE) std::atomic<bool> m_bIsReading;
     alignas(MPSCQueue::CACHE_LINE_SIZE) std::atomic<bool> m_bIsWriting;
 
-    std::vector<T> m_vecCircularBuffer;
+    std::vector<std::unique_ptr<MPSCQueue::Slot>> m_vecCircularBuffer;
 };
